39fbdd4b56df4af4dba2a62f20b1bf5c3cb90a51
wnt <196152738+zel743@users.noreply.github.com>
2025-08-05 14:58:19 -0600

    fic

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include  <stdlib.h>
 #include <string.h>
 
+#define max_paises 30
 
 struct estado {
     char nombre [50];
@@ -11,61 +12,214 @@ struct estado {
 
 struct pais {
     char nombre [40];
-    struct estado *estados;  
-    struct pais *siguiente;
+    struct estado *primer;
+    struct estado *ultimo;
 
 };
 
-struct pais *listaPaises = NULL;
+struct pais paises [max_paises];
+int paisesTodos = 0;
+
+//funcion que se va a usar para buscar
+int buscar( char *nombre) {
+
+for (int i = 0; i < paisesTodos; i++)
+    {
+    if (strcmp (paises[i].nombre, nombre) == 0)
+        return i;
+}
+return -1;
+};
 
 
+//funcion para enlistar los estados de los paises
+void imprimirlista() {
+char nombrepais[40];
+    int ind = 0;
+    printf("ingrese el nombre del pais: \n");
+    scanf(" %[^\n]", nombrepais);
 
-int imprimirlista() {
+
+    ind = buscar(nombrepais);
+    if (ind == -1) {
+
+        printf("el pais no existe: \n");
+        getchar();
+        return;
+    }
+    printf("\nEstados de %s:\n", nombrepais);
+struct estado *aux = paises[ind].primer;
+    while (aux != NULL) {
+        printf("- %s\n", aux->nombre);
+        aux = aux->siguiente;
+    }
+
+getchar();
+};
+
+//funcion para agregar un estado nuevo en un pais
+void nuevoEstado() {
+char nombrepais[50];
+    printf("ingrese el nombre del pais: \n");
+    scanf(" %[^\n]", nombrepais);
+    int ind = buscar(nombrepais);
+    if (ind == -1) {
+        printf("el pais no existe: \n");
+        getchar();
+        return;
+    }
+
+    struct estado *nuevo = (struct estado*)malloc(sizeof(struct estado));
+    if (nuevo == NULL) {
+        printf("\nNo hay suficiente espacio!\n");
+        getchar();
+        return;
+    }
+
+    printf("Ingrese el nombre del nuevo estado: ");
+    scanf(" %[^\n]", nuevo->nombre);
+    nuevo->siguiente = NULL;
+
+    if (paises[ind].primer == NULL) {
+        paises[ind].primer = nuevo;
+        paises[ind].ultimo = nuevo;
+    } else {
+        paises[ind].ultimo->siguiente = nuevo;
+        paises[ind].ultimo = nuevo;
+    }
+
+    printf("Estado '%s' agregado a '%s'.\n", nuevo->nombre, nombrepais);
+    getchar();
 
 };
 
-int nuevoEstado()
-{};
+//funcion eliminar estado
 
-int eliminarEstado()
-{};
-int nuevoPais() {
-    int x;
-    char nombrePais[40];
-    printf("Ingrese el nombre del nuevo país: ");
+int eliminarEstado() {
+
+    char nombrePais[40], nombreEstado[50];
+    int ind = 0;
+
+    printf("ingrese el nombre del país: ");
     scanf(" %[^\n]", nombrePais);
+    printf("ingrese el nombre del estado a eliminar: ");
+    scanf(" %[^\n]", nombreEstado);
 
-    struct pais *nuevoPais = (struct pais*)malloc(sizeof(struct pais));
-    strcpy(nuevoPais->nombre, nombrePais);
-    nuevoPais->estados = NULL;
-    nuevoPais->siguiente = NULL;
-// aqui se agregan los estados 
-    do {
+    ind = buscar(nombrePais);
+    if (ind == -1) {
+        printf("Error: El país no existe.\n");
+        getchar();
+        return;
+    }
 
-        char nombreEstado[50];
-        printf("Ingrese un estado: ");
-        scanf(" %[^\n]", nombreEstado);
-        struct estado *nuevoEstado = (struct estado*)malloc(sizeof(struct estado));
-        strcpy(nuevoEstado->nombre, nombreEstado);
-        nuevoEstado->siguiente = NULL;
+    struct estado *actual = paises[ind].primer;
+    struct estado *anterior = NULL;
 
-        if (nuevoPais->estados == NULL) {
-            nuevoPais->estados = nuevoEstado;
-        } else {
-            struct estado *ultimo = nuevoPais->estados;
-            while (ultimo->siguiente != NULL) {
-                ultimo = ultimo->siguiente;
-            }
-            ultimo->siguiente = nuevoEstado;
+    while (actual != NULL && strcmp(actual->nombre, nombreEstado) != 0) {
+        anterior = actual;
+        actual = actual->siguiente;
+    }
+
+
+    if (actual == NULL) {
+        printf("error: el estado no existe en este país.\n");
+        getchar();
+        return;
+    }
+
+
+    if (anterior == NULL) {
+        paises[ind].primer = actual->siguiente;
+        if (actual->siguiente == NULL) {
+            paises[ind].ultimo = NULL;
         }
-        printf("desea continuar? si = 1");
+    } else {
+        anterior->siguiente = actual->siguiente;
+        if (actual->siguiente == NULL) {
+            paises[ind].ultimo = anterior;
+        }
+    }
 
-    }while (x != 0);
+
+    free(actual);
+    printf("Estado eliminado \n");
+    getchar();
+
+
+
+
+};
+//funcion nuevo pais
+void nuevoPais() {
+    int x = 0;
+    printf("ingrese el nombre del pais: \n");
+    scanf(" %[^\n]", paises[paisesTodos].nombre);
+
+    if (buscar(paises[paisesTodos].nombre) != -1) {
+        printf("pais ya existente\n");
+        getchar();
+        return;
+    }
+
+    paises[paisesTodos].primer= NULL;
+    paises[paisesTodos].ultimo = NULL;
+
+    do {
+        struct estado *nuevo = (struct estado*)malloc(sizeof(struct estado));
+        if (nuevo == NULL) {
+            printf("\nno hay suficiente espacio!\n");
+            getchar();
+            break;
+        }
+
+        printf("ingrese un estado: ");
+        scanf(" %[^\n]", nuevo->nombre);
+        nuevo->siguiente = NULL;
+
+        if (paises[paisesTodos].primer== NULL) {
+            paises[paisesTodos].primer = nuevo;
+            paises[paisesTodos].ultimo = nuevo;
+        } else {
+            paises[paisesTodos].ultimo->siguiente = nuevo;
+            paises[paisesTodos].ultimo = nuevo;
+        }
+
+        printf("agregar otro estado = 1, salir = 0: ");
+        scanf(" %d", &x);
+    } while (x == 1);
+
+    paisesTodos++;
+    printf("País '%s' agregado con sus estados.\n", paises[paisesTodos-1].nombre);
+    getchar();
 
 };
 
-int dardeBaja()
-{};
+int dardeBaja() {
+char nombrepais[50];
+    int ind = 0;
+    if (paisesTodos == 0) {
+        printf("no hay países\n");
+        getchar();
+        return;
+    }
+    printf("ingresa el nombre de pais a borrar: ");
+    scanf(" %[^\n]", nombrepais);
+    ind = buscar(nombrepais);
+    if (ind == -1) {
+
+        printf("el pais no existe: \n");
+        getchar();
+        return;
+    }
+    struct estado *aux = paises[ind].primer;
+    while (aux != NULL) {
+        struct estado *temp = aux;
+        aux = aux->siguiente;
+        free(temp);
+    }
+paisesTodos--;
+    printf("pais eliminado: ");
+};
 
 
 
@@ -76,7 +230,7 @@ void main () {
     do {
         printf("menu\n");
         printf("1. Imprimir lista de estados de un pais\n");
-        printf("2. Insertar nuevo estado dado un pais\n");
+        printf("2. Insertar nuevo estado en un pais\n");
         printf("3. Eliminar un estado dado un pais\n");
         printf("4. Dar de alta un nuevo pais, con sus estados\n");
         printf("5. Dar de baja un pais con todos sus estados\n");
